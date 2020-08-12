99f6068588784dc134887aff712224c694cee001
Aiswarya Krishnan <aswathikrishnan204@gmail.com>
2020-08-12 10:15:01 +0530

    done

---
+++ testimonials/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { BodyComponent } from './body/body.component';
+import { FeedComponent } from './feed/feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    BodyComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
